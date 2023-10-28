73cb4f2c9dcc3135ac09c61fb5341a5c864bf15a
Nimsaramahagedara <nimsaramahagedara@gmail.com>
2023-10-28 11:31:21 +0530

    corsEnable

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ import dbConfig from './util/dbConfig.js';
 import userRouter from './routes/userRotes.js';
 import pharmacyRouter from './routes/pharmacyRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
-
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 app.use('/', userRouter);
